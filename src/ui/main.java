7540a4057591a29a93c0acf304c327458f227e30
Sonny Young <sonnyy99@gmail.com>
2019-07-04 21:47:09 -0700

    PP1 - Basic

---
+++ src/ui/main.java
@@ -0,0 +1,18 @@
+package ui;
+
+public class main {
+
+    public static void main(String args[]) {
+        factorial();
+        message();
+    }
+
+    public static void factorial() {
+        System.out.println("3! is " + 1*2*3);
+    }
+
+    public static void message() {
+        System.out.println("Goodbye!");
+    }
+}
+
