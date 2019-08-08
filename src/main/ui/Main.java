26284244ade24f0b16025e15c1939fff49d0d912
Sonny Young <sonnyy99@gmail.com>
2019-08-08 10:38:52 -0700

    PP11 - Changed main menu font and added buttons instead of text input to choose options

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,10 +1,10 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,8 +12,6 @@ import java.io.IOException;
 
 
 public class Main extends Application {
-
-    private Button enterButton;
 
     public static void main(String[] args) throws IOException {
         FountainLocations fl = new FountainLocations();
@@ -30,54 +28,27 @@ public class Main extends Application {
         mainStage.setTitle("Water Fountains of UBC");
 
         Label mainMenu = new Label("MAIN MENU");
-        Label addLabel = new Label("Enter [1] to add a water fountain");
-        Label removeLabel = new Label("Enter [2] to remove a water fountain");
-        Label printAllLabel = new Label("Enter [3] to display all water fountains");
-        Label printBuildingLabel = new Label("Enter [4] to display all fountains in a building");
+        mainMenu.setStyle("-fx-font-size: 24");
 
-        TextField inputString = new TextField();
+        Button addFountainButton = new Button("Add a Water Fountain");
+        Button removeFountainButton = new Button("Remove a Water Fountain");
+        Button printAllFountainsButton = new Button("Display all Water Fountains");
+        Button printBuildingFountainsButton = new Button("Find all Water Fountains in a Building");
 
-        enterButton = new Button("Click here to enter your choice");
-        enterButton.setOnAction(e -> isValid(inputString.getText()));
+        addFountainButton.setOnAction(e -> AddFountainWindow.display());
+        removeFountainButton.setOnAction(e -> RemoveFountainWindow.display());
+        printAllFountainsButton.setOnAction(e -> ShowAllFountainsWindow.display());
+        printBuildingFountainsButton.setOnAction(e -> ShowBuildingFountainsWindow.display());
 
         VBox layout = new VBox(20);
-        layout.getChildren().addAll(mainMenu, addLabel, removeLabel, printAllLabel,
-                printBuildingLabel, inputString, enterButton);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(mainMenu, addFountainButton, removeFountainButton,
+                printAllFountainsButton, printBuildingFountainsButton);
 
-        Scene scene = new Scene(layout, 500, 300);
+        Scene scene = new Scene(layout, 300, 300);
         mainStage.setScene(scene);
         mainStage.show();
 
     }
-
-    private Boolean isValid(String text) {
-        try {
-            int choice = Integer.parseInt(text);
-            if (choice < 0 || choice > 4) {
-                throw new NumberFormatException();
-            }
-            chooseOptions(choice);
-
-            return true;
-        } catch (NumberFormatException e) {
-            PopupWindow.display("ERROR", "You must enter an integer between 1 and 4");
-            return false;
-        }
-    }
-
-    private void chooseOptions(int choice) {
-        if (choice == 1) {
-            AddFountainWindow.display();
-        }
-        if (choice == 2) {
-            RemoveFountainWindow.display();
-        }
-        if (choice == 3) {
-            ShowAllFountainsWindow.display();
-        }
-        if (choice == 4) {
-            ShowBuildingFountainsWindow.display();
-        }
-    }
-
 }
+
